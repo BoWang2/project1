d878c3e565ab4a0968faf5039670995781e851a5
bo wang <bw254@afsaccess1.njit.edu>
2017-10-23 10:52:23 -0400

    remove useless word

--- classes/homepage.php
+++ classes/homepage.php
@@ -3,8 +3,10 @@ class homepage extends page
 {
 	public function get()
 	{
-		$this->html .= stringfunction::printThis("Welcome");
-		header('Location:index.php?page=upload');
+		//$this->html .= stringfunction::printThis("Welcome");
+		//if(time_nanosleep(3,500000000) === true)
+		header('Location:index.php?page=upload');//tansfer user to page of upload
+
 	}
     
 }
